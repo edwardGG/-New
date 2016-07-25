e73422ba021d30494241d8d3cb38400830094959
edwardGG <6954771919@qq.com>
2016-07-25 09:53:39 +0800

    Initial Commit

---
+++ 群体上报New/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  群体上报New
+//
+//  Created by Edward on 16/7/25.
+//  Copyright © 2016年 Edward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 群体上报New/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  群体上报New
+//
+//  Created by Edward on 16/7/25.
+//  Copyright © 2016年 Edward. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
